137972650b9c27c54133c4bce4a1d90eaec7c053
Kawtious <mewmewby2@gmail.com>
2023-11-28 00:58:49 -0700

    Remove API test pages and scripts

--- webpack.config.js
+++ webpack.config.js
@@ -10,28 +10,7 @@ module.exports = {
         home: './src/scripts/pages/Home.script.ts',
         registerUser: './src/scripts/pages/auth/RegisterUser.script.ts',
         loginUser: './src/scripts/pages/auth/LoginUser.script.ts',
-        careersMenu: './src/scripts/pages/menus/CareersMenu.script.ts',
-        insertCareer: './src/scripts/pages/api/careers/InsertCareer.script.ts',
-        updateCareer: './src/scripts/pages/api/careers/UpdateCareer.script.ts',
-        deleteCareer: './src/scripts/pages/api/careers/DeleteCareer.script.ts',
-        insertCourse: './src/scripts/pages/api/courses/InsertCourse.script.ts',
-        updateCourse: './src/scripts/pages/api/courses/UpdateCourse.script.ts',
-        deleteCourse: './src/scripts/pages/api/courses/DeleteCourse.script.ts',
-        insertProfessor:
-            './src/scripts/pages/api/professors/InsertProfessor.script.ts',
-        updateProfessor:
-            './src/scripts/pages/api/professors/UpdateProfessor.script.ts',
-        deleteProfessor:
-            './src/scripts/pages/api/professors/DeleteProfessor.script.ts',
-        insertProfessorEvent:
-            './src/scripts/pages/api/professorEvents/InsertProfessorEvent.script.ts',
-        updateProfessorEvent:
-            './src/scripts/pages/api/professorEvents/UpdateProfessorEvent.script.ts',
-        deleteProfessorEvent:
-            './src/scripts/pages/api/professorEvents/DeleteProfessorEvent.script.ts',
-        insertTerm: './src/scripts/pages/api/terms/InsertTerm.script.ts',
-        updateTerm: './src/scripts/pages/api/terms/UpdateTerm.script.ts',
-        deleteTerm: './src/scripts/pages/api/terms/DeleteTerm.script.ts'
+        careersMenu: './src/scripts/pages/menus/CareersMenu.script.ts'
     },
     module: {
         rules: [
